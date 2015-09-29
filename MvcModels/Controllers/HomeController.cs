6d995016fe55d9e7e06224b3dc54f8c0ae0b41a6
Kimberly Davis <kwatsondavis@gmail.com>
2015-09-29 10:33:01 -0400

    Initial Commit

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using MvcModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] personData =
+        {
+            new Person {PersonId = 1, FirstName = "Adam", LastName = "Freeman",
+                Role = Role.Admin},
+            new Person {PersonId = 2, FirstName = "Jacqui", LastName = "Freeman",
+                Role = Role.User},
+            new Person {PersonId = 3, FirstName = "John", LastName = "Smith",
+                Role = Role.User},
+            new Person {PersonId = 4, FirstName = "Anne", LastName = "Jones",
+                Role = Role.Guest}
+        };
+
+        public ActionResult Index(int id = 1)
+        {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+            return View(dataItem);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person model)
+        {
+            return View("Index", model);
+        }
+
+        public ActionResult DisplaySummary([Bind(Prefix="HomeAddress", Exclude = "Country")] AddressSummary summary)
+        {
+            return View(summary);
+        }
+
+        public ActionResult Names(IList<string> names)
+        {
+            names = names ?? new List<string>();
+            return View(names);
+        }
+
+        public ActionResult Address(FormCollection formData)
+        {
+            IList<AddressSummary> addresses = new List<AddressSummary>();
+            if (TryUpdateModel(addresses, formData))
+            {
+                // proceed as normal
+            } else
+            {
+                // provide feedback to user
+            }
+            
+            return View(addresses);
+        }
+    }
+}
